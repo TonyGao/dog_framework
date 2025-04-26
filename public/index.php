a4a57bf2a7b3ebae87679bb33dc8eb2bba3dc88c
TonyGao <86developer@gmail.com>
2025-04-26 11:45:08 +0800

    feat(view_editor): 添加表格组件，目前的规划主要是用于布局，为自定义表单视图做准备。
    * 增加视图编辑器上方的toolbar静态html，为表格布局格式化做准备。目前选取多单元格、表格默认宽度还有问题，属于早期开发提交版本，随后分多次完善和修复。
    * 修改了框架public/index.php 用于隐藏高版本php会造成第一次显示警告的问题。

--- public/index.php
+++ public/index.php
@@ -5,5 +5,10 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    // 现在 $context['APP_ENV'] 和 $context['APP_DEBUG'] 已经可以用了
+    if ($context['APP_ENV'] === 'dev') {
+        error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+    }
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
